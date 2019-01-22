cc64cf5fce61aa03365d18a8529374b1fc91c50c
Alessandra Escalante <escalana@bc.edu>
2019-01-22 11:38:11 -0500

    First commit

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Code below executes when the app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("The View Loaded!") //
     }
 
 
